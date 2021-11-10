9a12f9570c757ffb8131e58dc1969cd5bb1e50e9
RyanJohnson1612 <ryanjohnson@rogers.com>
2021-11-10 00:10:51 +0000

    WIP generate ticks based on user ticks value

--- src/js/main.js
+++ src/js/main.js
@@ -7,23 +7,52 @@ const drawBarChart = (data, options, element) => {
   const barWidth = (width/numItems) - options.barOptions.spacing - (options.barOptions.spacing/numItems);
 
   //create bar chart and append to element parameter
-  let chart = `<div class="bar-chart"><div class="inner-chart" style="height:${height}px;width:${width}px;"></div></div>`
+  let chart = `<div class="bar-chart-container">
+    <div class="y-axis-container" style="height:${height + 2}px;">
+      <div class="ticks-container"></div>
+    </div>
+    <div class="inner-chart" style="height:${height}px;width:${width}px;"></div>
+  </div>`
   $(chart).appendTo(el);
 
-  //loop through data parameter and create bars
+  //loop through data parameter and find height of bar then create it
   for(let i = 0; i < numItems; i++) {
     let barHeight = (data[i] * 100) / maxY;
-    createBar(options.barOptions, barHeight, barWidth, options.dataLabels[i]);
+    createBar(options.barOptions, barHeight, barWidth, options.dataLabels[i], options.barOptions.color[i]);
   }
+
+  createTicks(maxY, options.ticks, height, width);
 }
 
 //create bar and label and append to chart
-const createBar = (options, height, width, label) => {
-  let bar = `<div class="bar" style="height:${height}%;width:${width};background-color:${options.color};margin-left:${options.spacing}px">
+const createBar = (options, height, width, label, color) => {
+  let bar = `<div class="bar" style="height:${height}%;width:${width};background-color:${color};margin-left:${options.spacing}px">
     <div class="bar-label">${label}</div>
   </div>`;
 
-  $(bar).appendTo('.inner-chart')
+  $(bar).appendTo('.inner-chart');
+}
+
+const createTicks = (max, ticks, height, width) => {
+  const numTicks = Math.floor(max/every) + 1;
+  const tickSpacing = height/(numTicks - 1) - 12;
+
+  for(let i = 0; i < numTicks; i++) {
+    if(i < numTicks - 1) {
+      let tick = `<div class="tick" style="margin-top:${tickSpacing}px">
+        <span class="tick-number">${i*ticks}</span>
+        <span class="tick-dash" style="width:${width + 10}px"></span>
+      </div>`;
+      $('.ticks-container').prepend(tick);
+    }
+    else {
+      let tick = `<div class="tick">
+        <span class="tick-number">${i*ticks}</span>
+        <span class="tick-dash" style="width:${width + 10}px"></span>
+      </div>`;
+      $('.ticks-container').prepend(tick);
+    }
+  }
 }
 
 //Find the max value for the y axis
